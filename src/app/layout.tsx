d1525b6c961ad9cf5b3780e1a485433fa35a4f46
xJoyokz <jaya@itsavirus.com>
2024-11-14 19:51:47 +0800

    feat: Fix import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "@/styles/globals.css"
-import Image from "next/image";
 
 const nunito = localFont({
   src: "./fonts/Nunito-Regular.otf",
