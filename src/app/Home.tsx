ba3b149843b253603fd5720986a1765d9a23252b
xJoyokz <jaya@itsavirus.com>
2024-11-18 17:04:01 +0800

    feat: Add loader

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,36 +1,64 @@
-
 "use client";
 
+import { useState, useEffect } from "react";
 import Image from "next/image";
 
 import BorderSide from "@/components/side/side";
 import ImageCarousel from "@/components/slider/slider";
+import { Loader } from "@/components/loader/loader";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [imagesLoaded, setImagesLoaded] = useState(0);
+  const totalImages = 2; 
+
+
+  const handleImageLoad = () => {
+    setImagesLoaded((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+
+    if (imagesLoaded === totalImages) {
+      const timeout = setTimeout(() => {
+        setIsLoading(false);
+      }, 3000);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [imagesLoaded, totalImages]);
 
   return (
-    <div className="flex justify-center items-center relative">
-      <div>
-        <ImageCarousel />
+    <>
+      {isLoading && <Loader preset="centered" />}
+      <div
+        className={`flex justify-center items-center relative ${
+          isLoading ? "opacity-0" : "opacity-100 transition-opacity duration-500"
+        }`}
+      >
+        <div>
+          <ImageCarousel />
+        </div>
+        <BorderSide />
+        <Image
+          src="/img/Character-3.svg"
+          layout="intrinsic"
+          width={250}
+          height={580}
+          alt="Char 3"
+          className="absolute -right-1 -z-20 size-char-3 -top-5 translate-x-44 -rotate-12"
+          onLoadingComplete={handleImageLoad}
+        />
+        <Image
+          src="/img/Character-2.svg"
+          layout="intrinsic"
+          width={390}
+          height={800}
+          alt="Char 2"
+          className="absolute -bottom-28 z-20 right-0 translate-x-40"
+          onLoadingComplete={handleImageLoad}
+        />
       </div>
-      <BorderSide />
-      <Image
-        src="/img/Character-3.svg"
-        layout="intrinsic"
-        width={250}
-        height={580}
-        alt="Char 3"
-        className="absolute -right-1 -z-20 size-char-3 -top-5 translate-x-44 -rotate-12"
-      />
-      <Image
-        src="/img/Character-2.svg"
-        layout="intrinsic"
-        width={390}
-        height={800}
-        alt="Char 2"
-        className="absolute -bottom-28 z-20 right-0 translate-x-40"
-      />
-    </div>
-
+    </>
   );
 }
---
+++ src/components/loader/loader.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+import dynamic from "next/dynamic";
+
+
+
+type LoaderProps = {
+  preset?: "centered" | "cover-page";
+};
+
+export const Loader = ({ preset = "centered" }: LoaderProps) => {
+  useEffect(() => {
+    let animation: any;
+
+    const initializeLottie = async () => {
+      const container = document.querySelector("#loader-animation");
+      if (container) {
+        const lottieInstance = await import("lottie-web"); 
+        animation = lottieInstance.default.loadAnimation({
+          container: container as Element,
+          path: "/img/loader.json", 
+          renderer: "svg",
+          loop: true,
+          autoplay: true,
+        });
+      }
+    };
+
+    initializeLottie();
+
+    return () => {
+      if (animation) {
+        animation.destroy();
+      }
+    };
+  }, []);
+
+  return (
+    <div className={`preloader ${preset}`}>
+      <div id="loader-animation" />
+    </div>
+  );
+};
