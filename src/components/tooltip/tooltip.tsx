efe5eaca58828d9e8db91a9a49c932cee196cc00
xJoyokz <jaya@itsavirus.com>
2024-11-21 00:10:05 +0800

    feat: Create component toolttip

--- src/components/tooltip/tooltip.tsx
+++ src/components/tooltip/tooltip.tsx
@@ -14,7 +14,7 @@ const Tooltip: React.FC<TooltipProps> = ({ text, alignment = "left", type= "sosm
       <div
         className={`absolute top-1/2 transform -translate-y-1/2  whitespace-nowrap opacity-0 pointer-events-none transition-opacity duration-200 group-hover:opacity-100 group-hover:pointer-events-auto ${
           alignment !== "right" ? "left-auto right-full mr-5" : "left-full ml-5"
-        } ${type === "sosmed" ? "bg-black px-2 py-1 rounded-xl" : "bg-coklat px-2 py-3 rounded-xl"}`}
+        } ${type === "sosmed" ? "bg-black px-2 py-1 rounded-xl" : "bg-coklat px-3  rounded-xl"}`}
       >
         <text className={`${type === "sosmed"? "text-coklat text-base" : "text-black text-xs" }`}>{text}</text>
       </div>
