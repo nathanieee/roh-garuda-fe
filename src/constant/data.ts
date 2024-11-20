b91734d2b34de8dde40b93d7420c8ab9ddeedd84
xJoyokz <jaya@itsavirus.com>
2024-11-21 00:09:34 +0800

    update: Update data dummy

--- src/constant/data.ts
+++ src/constant/data.ts
@@ -1,29 +1,20 @@
-export const DATA =  [
-        {
-          "id": 1,
-          "imageUrl": "/img/test.png",
-          "alt": "image1"
-        },
-        {
-          "id": 2,
-          "imageUrl": "/img/test-1.png",
-          "alt": "image2"
-        },
-        {
-          "id": 3,
-          "imageUrl": "/img/test-2.png",
-          "alt": "image2"
-        },
-        {
-          "id": 4,
-          "imageUrl": "/img/ONE.jpg",
-          "alt": "image2"
-        },
-        {
-          "id": 5,
-          "imageUrl": "/img/test.png",
-          "alt": "image2"
-        },
-      ]
-    
-  
+export const DATA = [
+
+  {
+    "id": 2,
+    "imageUrl": "/img/test-1.png",
+    "alt": "image2"
+  },
+  {
+    "id": 3,
+    "imageUrl": "/img/test-2.png",
+    "alt": "image2"
+  },
+  {
+    "id": 4,
+    "imageUrl": "/img/ONE.jpg",
+    "alt": "image2"
+  },
+]
+
+
