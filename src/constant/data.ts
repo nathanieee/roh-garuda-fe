3f6a2cee6d438952fda9b80297ebe5a8b5af8411
xJoyokz <jaya@itsavirus.com>
2024-11-21 16:36:37 +0800

    feat: Add image for preview game

--- src/constant/data.ts
+++ src/constant/data.ts
@@ -1,19 +1,29 @@
 export const DATA = [
 
   {
+    "id": 1,
+    "imageUrl": "/img/slider-1.jpg",
+    "alt": "image1"
+  },
+  {
     "id": 2,
-    "imageUrl": "/img/test-1.png",
+    "imageUrl": "/img/slider-2.jpg",
     "alt": "image2"
   },
   {
     "id": 3,
-    "imageUrl": "/img/test-2.png",
-    "alt": "image2"
+    "imageUrl": "/img/slider-3.jpg",
+    "alt": "image3"
   },
   {
     "id": 4,
-    "imageUrl": "/img/ONE.jpg",
-    "alt": "image2"
+    "imageUrl": "/img/slider-4.jpg",
+    "alt": "image4"
+  },
+  {
+    "id": 5,
+    "imageUrl": "/img/slider-5.jpg",
+    "alt": "image5"
   },
 ]
 
